d5b41ca65a3bfa15f617b96c564a0fb0ea014189
A00214672 <petter.fahlander@atlascopco.com>
2024-01-03 15:35:15 +0100

    gogo

--- game.py
+++ game.py
@@ -4,19 +4,17 @@ import pygame
 
 
 def display_score():
-        current_time = int(pygame.time.get_ticks() / 1000) - start_time
-        score_surface = test_font.render(str(current_time), True, ('green'))
-        score_rect = score_surface.get_rect(center=(350, 50))
-        screen.blit(score_surface, score_rect)
-
-
+    current_time = int(pygame.time.get_ticks() / 1000) - start_time
+    score_surface = game_font.render(str(current_time), True, ('green'))
+    score_rect = score_surface.get_rect(center=(350, 50))
+    screen.blit(score_surface, score_rect)
 
 
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
-test_font = pygame.font.Font(
+game_font = pygame.font.Font(
     'Intro/font/Pixeltype.ttf', 50)  # Remove leading slash
 game_active = True
 start_time = 0
@@ -34,8 +32,15 @@ player_rect = player_surface.get_rect(midbottom=(80, 300))
 player_gravity = 0
 
 # Intro screen
-player_stand = pygame.image.load('Intro/graphics/Player/player_stand.png').convert_alpha()
+player_stand = pygame.image.load(
+    'Intro/graphics/Player/player_stand.png').convert_alpha()
+player_stand = pygame.transform.scale2x(player_stand)  # Scaled
 player_stand_rect = player_stand.get_rect(center=(400, 200))
+game_message = game_font.render(
+    'Press SPACE to start!', True, ('black'))
+game_message_rect = game_message.get_rect(center=(400, 300))
+game_name_rect = game_message.get_rect(center=(450, 100))
+game_name = game_font.render('Pixel Runner', True, ('green'))
 
 
 while True:
@@ -57,7 +62,7 @@ while True:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
                 start_time = int(pygame.time.get_ticks() / 1000)
- 
+
     if game_active:
         #  Background
         screen.blit(sky_surface, (0, 0))
@@ -65,6 +70,7 @@ while True:
 # Collision
         if player_rect.colliderect(snail_rect):
             game_active = False
+            snail_rect.left = 800
 
 # Player
         player_gravity += 0.2
@@ -81,8 +87,7 @@ while True:
     else:
         screen.fill((94, 129, 162))
         screen.blit(player_stand, player_stand_rect)
-        snail_rect.left = 800
-
-
+        screen.blit(game_message, game_message_rect)
+        screen.blit(game_name, game_name_rect)
     pygame.display.update()
     clock.tick(60)
